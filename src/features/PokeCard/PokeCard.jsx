1147c883aac780861b51f570e88fd5668d136e9e
Valeriy-Sorochynskyi <haribol162@gmail.com>
2019-05-19 23:30:12 +0300

    some changes

--- src/features/PokeCard/PokeCard.jsx
+++ src/features/PokeCard/PokeCard.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class PokeCard extends Component {
   render() {
-    const { name, imgUrl, types, openDetailes, } = this.props;
+    const { name, imgUrl, types, openDetailes } = this.props;
     return (
-      <div onClick={openDetailes(this.props)} className="col-4 mb-3">
+      <div
+        onClick={openDetailes(this.props)}
+        className="col-lg-4 col-md-4 col-sm-6 col-8 mb-3"
+      >
         <div className="card cardwidth ">
-          {imgUrl ?
-            <img
-              src={`${imgUrl}`}
-              className="card-img-top"
-              alt={name}
-            /> : 'Loding...'}
+          {imgUrl ? (
+            <img src={`${imgUrl}`} className="card-img-top" alt={name} />
+          ) : (
+            "Loding..."
+          )}
           <div className="card-body">
             <div className="row justify-content-center">
               <p className="card-text mb-2">{name}</p>
@@ -19,14 +21,16 @@ class PokeCard extends Component {
             <div className="row justify-content-center">
               {types.map((type, i) => {
                 return (
-                  <span key={i} className="badge">{type}</span>
-                )
+                  <span key={i} className="badge">
+                    {type}
+                  </span>
+                );
               })}
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/features/PokeDetailes/PokeDetailes.jsx
+++ src/features/PokeDetailes/PokeDetailes.jsx
@@ -1,44 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class PokeDetailes extends Component {
   render() {
     const { detailes } = this.props;
     return (
-      <div className="row justify-content-center">
-        <div className="card card-with-detailes position-fixed ">
-          <img src={detailes.imgUrl} className="card-img-top" alt={detailes.name} />
-          <div className="row justify-content-center">
-            <h5 className="card-title">{detailes.name}</h5>
-          </div>
-          <div className="overflow">
-            <table className="table table-sm" >
-              <tbody>
-                <tr>
-                  <td>type</td>
-                  <td>{detailes.types.join(' ')}</td>
-                </tr>
-                {detailes.stats.map(item => {
-                  return (
-                    <tr key={item.statName}>
-                      <td>{item.statName}</td>
-                      <td>{item.stat}</td>
-                    </tr>
-                  )
-                })}
-                <tr>
-                  <td>weight</td>
-                  <td>{detailes.weight}</td>
-                </tr>
-                <tr>
-                  <td>total moves</td>
-                  <td>{detailes.totalMoves}</td>
-                </tr>
-              </tbody>
-            </table>
+      <div className="row justify-content-center sticky-top">
+        <div className="col-10">
+          <div className="card card-with-detailes">
+            <img
+              src={detailes.imgUrl}
+              className="card-img-top"
+              alt={detailes.name}
+            />
+            <div className="row justify-content-center">
+              <h5 className="card-title">{detailes.name}</h5>
+            </div>
+            <div>
+              <table className="table table-sm">
+                <tbody>
+                  <tr>
+                    <td>type</td>
+                    <td>{detailes.types.join(" ")}</td>
+                  </tr>
+                  {detailes.stats.map(item => {
+                    return (
+                      <tr key={item.statName}>
+                        <td>{item.statName}</td>
+                        <td>{item.stat}</td>
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    <td>weight</td>
+                    <td>{detailes.weight}</td>
+                  </tr>
+                  <tr>
+                    <td>total moves</td>
+                    <td>{detailes.totalMoves}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
