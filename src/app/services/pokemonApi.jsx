a5206cf877d2980756d7a8a01bc11e4fab8f86ab
Valeriy-Sorochynskyi <haribol162@gmail.com>
2019-05-19 23:30:12 +0300

    some changes

--- src/app/services/pokemonApi.jsx
+++ src/app/services/pokemonApi.jsx
@@ -1,34 +1,36 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=12';
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=12";
 
 const pockemonApi = {
-    getAll() {
-      return fetch(BASE_URL)
-        .then(response => response.json())
-        .then(data => {
-          return Promise.all(
-            data.results.map(result => {
-              return this.getOne(result.url);
-            })
-          );
-        })
-  
-    },
-  
-    getOne(url) {
-      return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          return {
-            id: data.id,
-            name: data.name,
-            imgUrl: data.sprites.front_default,
-            types: data.types.map(item => item.type.name),
-            stats: data.stats.map(item => ({ stat: item.base_stat, statName: item.stat.name })),
-            weight: data.weight,
-            totalMoves: data.moves.length,
-          }
-        })
-    }
-  }
+  getAll() {
+    return fetch(BASE_URL)
+      .then(response => response.json())
+      .then(data => {
+        return Promise.all(
+          data.results.map(result => {
+            return this.getOne(result.url);
+          })
+        );
+      });
+  },
 
-  export default pockemonApi;
+  getOne(url) {
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return {
+          id: data.id,
+          name: data.name,
+          imgUrl: data.sprites.front_default,
+          types: data.types.map(item => item.type.name),
+          stats: data.stats.map(item => ({
+            stat: item.base_stat,
+            statName: item.stat.name
+          })),
+          weight: data.weight,
+          totalMoves: data.moves.length
+        };
+      });
+  }
+};
+
+export default pockemonApi;
--- src/features/PokeDashboard/PokeDashboard.jsx
+++ src/features/PokeDashboard/PokeDashboard.jsx
@@ -1,70 +1,69 @@
-import React, { Component } from 'react';
-import PokeCard from '../PokeCard/PokeCard';
+import React, { Component } from "react";
+import PokeCard from "../PokeCard/PokeCard";
 import PokeDetailes from "../PokeDetailes/PokeDetailes";
 import pockemonApi from "../../app/services/pokemonApi";
 
 class PokeDashboard extends Component {
   state = {
     pokemons: [],
-    loadMoreUrl: 'https://pokeapi.co/api/v2/pokemon?offset=12&limit=12',
+    loadMoreUrl: "https://pokeapi.co/api/v2/pokemon?offset=12&limit=12",
     selectedCard: false,
-    detailes: {
-      types: [],
-      stats: []
-    },
-  }
+    detailes: {},
+    isLoadMoreActive: true
+  };
 
   componentDidMount() {
     pockemonApi.getAll().then(pokemons => {
       this.setState({
-        pokemons,
-      })
+        pokemons
+      });
     });
   }
 
   handleLoadMore = () => {
+    this.setState({ isLoadMoreActive: false });
     fetch(this.state.loadMoreUrl)
       .then(response => response.json())
       .then(data => {
         this.setState({
           selectedCard: false,
-          loadMoreUrl: data.next,
-        })
+          loadMoreUrl: data.next
+        });
         return Promise.all(
           data.results.map(result => {
             return pockemonApi.getOne(result.url);
           })
-        )
-
-      }).then(pokemons => {
-        this.setState((prevState) => {
+        );
+      })
+      .then(pokemons => {
+        this.setState(prevState => {
           return {
             ...prevState,
-            pokemons: [...prevState.pokemons, ...pokemons]
-          }
-        })
-      })
-  }
+            pokemons: [...prevState.pokemons, ...pokemons],
+            isLoadMoreActive: true,
+          };
+        });
+      });
+  };
 
-  handleOpenDetailes = (detailes) => () => {
+  handleOpenDetailes = detailes => () => {
     this.setState({
       selectedCard: true,
-      detailes: detailes,
-    })
-
-  }
+      detailes: detailes
+    });
+  };
 
   render() {
-    const { pokemons, selectedCard } = this.state;
+    const { pokemons, selectedCard, isLoadMoreActive } = this.state;
     return (
       <>
         <div className="row justify-content-center">
           <h1>Pokedex</h1>
         </div>
-        <div className="row">
-          <div className="col-8">
-            <div className="row">
-              {pokemons.map((pokemon) => {
+        <div className="row row justify-content-center">
+          <div className="col-6 col-lg-6">
+            <div className="row justify-content-center">
+              {pokemons.map(pokemon => {
                 return (
                   <PokeCard
                     key={pokemon.name}
@@ -76,19 +75,27 @@ class PokeDashboard extends Component {
                     stats={pokemon.stats}
                     openDetailes={this.handleOpenDetailes}
                   />
-                )
+                );
               })}
             </div>
             <div className="row justify-content-center">
-              <button onClick={this.handleLoadMore} type="button" className="btn btn-primary col-8">Load More</button>
+              {isLoadMoreActive ? (
+                <button
+                  onClick={this.handleLoadMore}
+                  type="button"
+                  className="btn btn-primary col-8"
+                >
+                  Load More
+                </button>
+              ) : 'Loading...'}
             </div>
           </div>
-          <div className="col-4">
+          <div className="col-6 col-lg-4">
             {selectedCard && <PokeDetailes detailes={this.state.detailes} />}
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
